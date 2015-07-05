7b0ccfa65a196100f84b4a6b1d052322590b73ad
yolles <yolles@adobe.com>
2015-07-04 17:52:52 -0700

    Enable logout
    Logout through a post to j_security_check and redirect. This will probably change
    to a custom servlet instead of client-side JavaScript.

--- ui/src/main/resources/jcr_root/etc/clientlibs/admin/js/admin.js
+++ ui/src/main/resources/jcr_root/etc/clientlibs/admin/js/admin.js
@@ -12,7 +12,20 @@ $(function() {
       ['misc', ['fullscreen', 'codeview']],
       ['group', ['help']]
     ]
- });
+  });
+
+  $('.logout').click(function(e){
+    e.preventDefault();
+    $.post('j_security_check', {
+      j_username : '-',
+      j_password : '-',
+      j_validate : true
+    }).always(function(data){
+      if (data.status === 403) {
+        window.location = '/content/admin/login.html'
+      }
+    });
+  });
 });
 
 var app = angular.module('publick', ['ngFileUpload']);
