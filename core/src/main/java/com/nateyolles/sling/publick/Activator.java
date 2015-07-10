6ccfa63c47b4689c224a931e2b50ee8c469a3ed9
yolles <yolles@adobe.com>
2015-07-10 14:15:34 -0700

    Create author and tester groups on activation
    Run in the Activator class. Check if groups "authors" and "testers" exist and create
    them if they don't. This way the groups will already be available to the admin when
    he starts creating users.

--- core/src/main/java/com/nateyolles/sling/publick/Activator.java
+++ core/src/main/java/com/nateyolles/sling/publick/Activator.java
@@ -1,14 +1,18 @@
 package com.nateyolles.sling.publick;
 
-import java.util.Dictionary;
-import java.util.Hashtable;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+import javax.jcr.ValueFactory;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceRegistration;
-
+import org.osgi.framework.ServiceReference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.apache.jackrabbit.api.JackrabbitSession;
+import org.apache.jackrabbit.api.security.user.Authorizable;
+import org.apache.jackrabbit.api.security.user.UserManager;
+import org.apache.sling.jcr.api.SlingRepository;
 
 public class Activator implements BundleActivator {
 
@@ -17,10 +21,51 @@ public class Activator implements BundleActivator {
     @Override
     public void start(BundleContext bundleContext) throws Exception {
         LOGGER.info(bundleContext.getBundle().getSymbolicName() + " started");
+
+        createGroups(bundleContext);
     }
 
     @Override
     public void stop(BundleContext bundleContext) throws Exception {
         LOGGER.info(bundleContext.getBundle().getSymbolicName() + " stopped");
+    }
+
+    private void createGroups(BundleContext bundleContext){
+        ServiceReference SlingRepositoryFactoryReference = bundleContext.getServiceReference(SlingRepository.class.getName());
+        SlingRepository repository = (SlingRepository)bundleContext.getService(SlingRepositoryFactoryReference);
+
+        Session session = null;
+
+        if (repository != null) {
+            try {
+                session = repository.loginAdministrative(null);
+
+                if (session != null && session instanceof JackrabbitSession) {
+                    UserManager userManager = ((JackrabbitSession)session).getUserManager();
+                    ValueFactory valueFactory = session.getValueFactory();
+
+                    Authorizable authors = userManager.getAuthorizable("authors");
+    
+                    if (authors == null) {
+                        authors = userManager.createGroup("authors");
+                        authors.setProperty("displayName", valueFactory.createValue("Authors"));
+                    }
+                  
+                    Authorizable testers = userManager.getAuthorizable("testers");
+    
+                    if (testers == null) {
+                        testers = userManager.createGroup("testers");
+                        testers.setProperty("displayName", valueFactory.createValue("Testers"));
+                    }
+                }
+            } catch (RepositoryException e) {
+                LOGGER.error("Could not get session.", e);
+            } finally {
+                if (session != null && session.isLive()) {
+                    session.logout();
+                    session = null;
+                }
+            }
+        }
     }
 }
