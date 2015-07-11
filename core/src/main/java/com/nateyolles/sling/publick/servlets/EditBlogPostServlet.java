d44c91295b82e48d837d6022cb22d58e7a95001e
yolles <yolles@adobe.com>
2015-07-10 21:42:06 -0700

    Redirect to blog list after saving blog

--- core/src/main/java/com/nateyolles/sling/publick/servlets/EditBlogPostServlet.java
+++ core/src/main/java/com/nateyolles/sling/publick/servlets/EditBlogPostServlet.java
@@ -3,40 +3,29 @@ package com.nateyolles.sling.publick.servlets;
 import com.nateyolles.sling.publick.PublickConstants;
 
 import org.apache.commons.lang.StringUtils;
-import org.apache.felix.scr.annotations.Reference;
 import org.apache.felix.scr.annotations.sling.SlingServlet;
 import org.apache.sling.api.request.RequestParameter;
 import org.apache.sling.api.request.RequestParameterMap;
+import org.apache.sling.api.resource.PersistenceException;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.resource.ModifiableValueMap;
-import org.apache.sling.api.resource.ResourceResolverFactory;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
 import org.apache.sling.api.servlets.SlingAllMethodsServlet;
-import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
 import org.apache.jackrabbit.commons.JcrUtils;
 import org.apache.sling.jcr.resource.JcrResourceUtil;
 
 import javax.jcr.Node;
 import javax.jcr.RepositoryException;
 import javax.jcr.nodetype.*;
-import javax.jcr.Session;
-import javax.print.attribute.standard.DateTimeAtCompleted;
 import javax.servlet.ServletException;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.security.PublicKey;
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.osgi.framework.Constants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -89,17 +78,20 @@ public class EditBlogPostServlet extends SlingAllMethodsServlet {
 
             resolver.commit();
             resolver.close();
-        } catch (javax.jcr.RepositoryException e) {
+
+            response.sendRedirect(PublickConstants.ADMIN_LIST_PATH + ".html");
+        } catch (RepositoryException e) {
             LOGGER.error("Could not save blog to repository.", e);
+            response.sendRedirect(request.getHeader("referer"));
+        } catch (PersistenceException e){
+            LOGGER.error("Could not save blog to repository.", e);
+            response.sendRedirect(request.getHeader("referer"));
         } finally {
             if (resolver != null && resolver.isLive()) {
                 resolver.close();
                 resolver = null;
             }
         }
-
-        response.getWriter().print("Success: " + blogPath);
-        response.setStatus(200);
     }
 
     /**
