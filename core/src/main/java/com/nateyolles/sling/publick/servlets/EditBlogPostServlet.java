0674901a35caa179a0506cb2ea3a56e2b0e6c199
yolles <yolles@adobe.com>
2015-07-11 20:23:15 -0700

    Edit blog without losing previously saved image
    Upon editing a blog, if the image value is null, check to see if the node
    already exists and update with the existing value. Incorporate the FileUpload
    service.

--- core/src/main/java/com/nateyolles/sling/publick/servlets/EditBlogPostServlet.java
+++ core/src/main/java/com/nateyolles/sling/publick/servlets/EditBlogPostServlet.java
@@ -1,15 +1,15 @@
 package com.nateyolles.sling.publick.servlets;
 
 import com.nateyolles.sling.publick.PublickConstants;
+import com.nateyolles.sling.publick.services.FileUploadService;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.felix.scr.annotations.Reference;
 import org.apache.felix.scr.annotations.sling.SlingServlet;
-import org.apache.sling.api.request.RequestParameter;
-import org.apache.sling.api.request.RequestParameterMap;
 import org.apache.sling.api.resource.PersistenceException;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.resource.ModifiableValueMap;
+import org.apache.sling.api.resource.ValueMap;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
 import org.apache.sling.api.servlets.SlingAllMethodsServlet;
@@ -17,7 +17,6 @@ import org.apache.jackrabbit.JcrConstants;
 import org.apache.jackrabbit.api.JackrabbitSession;
 import org.apache.jackrabbit.api.security.user.Authorizable;
 import org.apache.jackrabbit.api.security.user.UserManager;
-import org.apache.jackrabbit.commons.JcrUtils;
 import org.apache.sling.jcr.resource.JcrResourceConstants;
 import org.apache.sling.jcr.resource.JcrResourceUtil;
 
@@ -28,7 +27,6 @@ import javax.jcr.nodetype.*;
 import javax.servlet.ServletException;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -41,6 +39,9 @@ public class EditBlogPostServlet extends SlingAllMethodsServlet {
     private static final Logger LOGGER = LoggerFactory.getLogger(EditBlogPostServlet.class);
     private static final String BLOG_PATH = "/%d/%02d";
     private static final String BLOG_ROOT = "blog";
+
+    @Reference
+    private FileUploadService fileUploadService;
 
     @Override
     protected void doPost(SlingHttpServletRequest request, SlingHttpServletResponse response) throws ServletException, IOException {
@@ -57,7 +58,13 @@ public class EditBlogPostServlet extends SlingAllMethodsServlet {
         final long year = Long.parseLong(request.getParameter("year"));
         final String path = String.format(BLOG_PATH, year, month);
         final String blogPath = PublickConstants.BLOG_PATH + path + "/" + url;
-        final String image = saveImage(request);
+        String image = fileUploadService.uploadFile(request, PublickConstants.IMAGE_PATH);
+
+        Resource existingNode = resolver.getResource(blogPath);
+
+        if (image == null && existingNode != null) {
+            image = existingNode.adaptTo(ValueMap.class).get("image", String.class);
+        }
 
         Map<String, Object> properties = new HashMap<String, Object>();
         properties.put(JcrConstants.JCR_PRIMARYTYPE, PublickConstants.NODE_TYPE_PAGE);
@@ -68,7 +75,10 @@ public class EditBlogPostServlet extends SlingAllMethodsServlet {
         properties.put("description", description);
         properties.put("month", month);
         properties.put("year", year);
-        properties.put("image", image != null ? image : StringUtils.EMPTY);
+
+        if (image != null) {
+            properties.put("image", image);
+        }
 
         if (keywords != null) {
             properties.put("keywords", keywords);
@@ -83,7 +93,6 @@ public class EditBlogPostServlet extends SlingAllMethodsServlet {
         }
 
         try {
-            Resource existingNode = resolver.getResource(blogPath);
 
             if (existingNode != null) {
                 ModifiableValueMap existingProperties = existingNode.adaptTo(ModifiableValueMap.class);
@@ -110,47 +119,5 @@ public class EditBlogPostServlet extends SlingAllMethodsServlet {
                 resolver = null;
             }
         }
-    }
-
-    /**
-     * Save uploaded image to the JCR under the default images node.
-     *
-     * @param request   the current SlingHTTPServletRequest
-     * @return          the image path, null if the image wasn't saved
-     */
-    private String saveImage(SlingHttpServletRequest request) {
-        final RequestParameterMap params = request.getRequestParameterMap();
-        ResourceResolver resolver = request.getResourceResolver();
-
-        String imagePath = null;
-
-        for (final Map.Entry<String, RequestParameter[]> pairs : params.entrySet()) {
-            final String key = pairs.getKey();
-            final RequestParameter[] pArr = pairs.getValue();
-            final RequestParameter param = pArr[0];
-
-            if (!param.isFormField()) {
-                final String name = param.getFileName();
-                final String mimeType = param.getContentType();
-
-                try {
-                    final InputStream stream = param.getInputStream();
-                    Resource imagesParent = resolver.getResource(PublickConstants.IMAGE_PATH);
-                    Node imageNode = JcrUtils.putFile(imagesParent.adaptTo(Node.class), name, mimeType, stream);
-                    resolver.commit();
-
-                    imagePath = imageNode.getPath();
-                } catch (javax.jcr.RepositoryException e) {
-                    LOGGER.error("Could not save image to repository.", e);
-                } catch (java.io.IOException e) {
-                    LOGGER.error("Could not get image input stream", e);
-                } finally {
-                    /* Exit loop after file is found. */
-                    break;
-                }
-            }
-        }
-
-        return imagePath;
     }
 }
