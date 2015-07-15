85e429508bede11544c9bebe4d0ecdce7d57ba82
yolles <yolles@adobe.com>
2015-07-15 08:41:49 -0700

    BlogEdit admin Sightly component cleanup

--- core/src/main/java/com/nateyolles/sling/publick/components/admin/BlogEdit.java
+++ core/src/main/java/com/nateyolles/sling/publick/components/admin/BlogEdit.java
@@ -1,25 +1,26 @@
 package com.nateyolles.sling.publick.components.admin;
 
-import java.util.Calendar;
-import java.util.Iterator;
-import java.util.List;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
+import java.util.List;
 
-import javax.jcr.query.Query;
 import javax.script.Bindings;
 
-import org.apache.sling.commons.json.JSONArray;
 import org.apache.sling.api.SlingHttpServletRequest;
-import org.apache.sling.api.SlingHttpServletResponse;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.resource.ValueMap;
 import org.apache.sling.api.scripting.SlingBindings;
+import org.apache.sling.commons.json.JSONArray;
 import org.apache.sling.scripting.sightly.pojo.Use;
 
-import com.nateyolles.sling.publick.PublickConstants;
-
+/**
+ * Sightly component to edit blog posts in the admin section. The
+ * component determines whether to create a new blog post or edit
+ * and existing blog post. To edit an existing blog post, pass
+ * the resource path in the URL as the suffix.
+ */
 public class BlogEdit implements Use {
 
     private Resource resource;
@@ -34,6 +35,11 @@ public class BlogEdit implements Use {
     private String content;
     private String description;
 
+    /**
+     * Sightly component initialization.
+     *
+     * @param bindings The current execution context.
+     */
     @Override
     public void init(Bindings bindings) {
         resource = (Resource)bindings.get(SlingBindings.RESOURCE);
@@ -43,6 +49,12 @@ public class BlogEdit implements Use {
         getBlog(path);
     }
 
+    /**
+     * Get the blog post properties if resource already exists otherwise
+     * set the month and year properties to the current date.
+     *
+     * @param path The resource path to the blog post.
+     */
     private void getBlog(String path) {
         ResourceResolver resolver = resource.getResourceResolver();
         Resource blog = resolver.getResource(path);
@@ -66,30 +78,65 @@ public class BlogEdit implements Use {
         }
     }
 
+    /**
+     * Get the title property.
+     *
+     * @return The title property.
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * Get the month property.
+     *
+     * @return The month property.
+     */
     public Long getMonth() {
         return month;
     }
 
+    /**
+     * Get the year property.
+     *
+     * @return The year property.
+     */
     public Long getYear() {
         return year;
     }
 
+    /**
+     * Get the resource name of the blog post URL.
+     *
+     * @return The resource name of hte blog post URL.
+     */
     public String getUrl() {
         return url;
     }
 
+    /**
+     * Get the visible property.
+     *
+     * @return The visible property.
+     */
     public boolean getVisible() {
         return visible;
     }
 
+    /**
+     * Get the multi-value keyword property.
+     *
+     * @return The multi-value keyword property.
+     */
     public String[] getKeywords() {
         return keywords;
     }
 
+    /**
+     * Get the multi-value keywords property as a JSON string.
+     *
+     * @return The multi-value keyword property as a JSON string.
+     */
     public String getKeywordsJSON() {
         JSONArray jsonArray = null;
 
@@ -102,18 +149,38 @@ public class BlogEdit implements Use {
         return jsonArray.toString();
     }
 
+    /**
+     * Get the image path property.
+     *
+     * @return The image path property.
+     */
     public String getImage() {
         return image;
     }
 
+    /**
+     * Get the blog post content.
+     *
+     * @return The blog post content.
+     */
     public String getContent() {
         return content;
     }
 
+    /**
+     * Get the description property.
+     *
+     * @return The description property.
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Get the month property.
+     *
+     * @return The month property.
+     */
     public List<Long> getMonths() {
         List<Long> months = new ArrayList<Long>();
         int length = 12;
@@ -125,6 +192,11 @@ public class BlogEdit implements Use {
         return months;
     }
 
+    /**
+     * Get the year property.
+     *
+     * @return the year property.
+     */
     public List<Long> getYears() {
         List<Long> years = new ArrayList<Long>();
         int preYears = 2;
