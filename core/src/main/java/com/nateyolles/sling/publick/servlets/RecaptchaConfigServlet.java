d383f5865f03a9186c0084cc866216a673abac22
yolles <yolles@adobe.com>
2015-07-14 21:06:12 -0700

    reCAPTCHA servlet cleanup and comments

--- core/src/main/java/com/nateyolles/sling/publick/servlets/RecaptchaConfigServlet.java
+++ core/src/main/java/com/nateyolles/sling/publick/servlets/RecaptchaConfigServlet.java
@@ -1,62 +1,66 @@
 package com.nateyolles.sling.publick.servlets;
 
 import com.nateyolles.sling.publick.PublickConstants;
-import com.nateyolles.sling.publick.services.FileUploadService;
 
-import org.apache.commons.lang.StringUtils;
-import org.apache.felix.scr.annotations.Reference;
 import org.apache.felix.scr.annotations.sling.SlingServlet;
-import org.apache.sling.api.request.RequestParameter;
-import org.apache.sling.api.request.RequestParameterMap;
 import org.apache.sling.api.resource.PersistenceException;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.resource.ModifiableValueMap;
-import org.apache.sling.api.resource.ResourceResolverFactory;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
 import org.apache.sling.api.servlets.SlingAllMethodsServlet;
-import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
-import org.apache.jackrabbit.commons.JcrUtils;
-import org.apache.sling.jcr.resource.JcrResourceUtil;
 
-import javax.jcr.Node;
-import javax.jcr.RepositoryException;
-import javax.jcr.nodetype.*;
-import javax.jcr.Session;
-import javax.print.attribute.standard.DateTimeAtCompleted;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.security.PublicKey;
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.osgi.framework.Constants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Post servlet to save reCAPTCHA config updates.
+ */
 @SlingServlet(paths = "/bin/recaptchaconfig")
 public class RecaptchaConfigServlet extends SlingAllMethodsServlet {
 
+    /**
+     * The logger.
+     */
     private static final Logger LOGGER = LoggerFactory.getLogger(RecaptchaConfigServlet.class);
 
+    /**
+     * The site key property of the reCAPTCHA config node.
+     */
+    private static final String SITE_KEY_PROPERTY = "siteKey";
+
+    /**
+     * The secret key property of the reCAPTCHA config node.
+     */
+    private static final String SECRET_KEY_PROPERTY = "secretKey";
+
+    /**
+     * The enabled property of the reCAPTCHA config node.
+     */
+    private static final String ENABLED_PROPERTY = "enabled";
+
+    /**
+     * Save reCAPTCHA properties on POST.
+     *
+     * @param request The Sling HTTP servlet request.
+     * @param response The Sling HTTP servlet response.
+     */
     @Override
     protected void doPost(SlingHttpServletRequest request, SlingHttpServletResponse response) throws ServletException, IOException {
-        final String siteKey = request.getParameter("siteKey");
-        final String secretKey = request.getParameter("secretKey");
-        final boolean enabled = Boolean.parseBoolean(request.getParameter("enabled"));
+        final String siteKey = request.getParameter(SITE_KEY_PROPERTY);
+        final String secretKey = request.getParameter(SECRET_KEY_PROPERTY);
+        final boolean enabled = Boolean.parseBoolean(request.getParameter(ENABLED_PROPERTY));
         final Map<String, Object> newProperties = new HashMap<String, Object>(){{
-            put("siteKey", siteKey);
-            put("secretKey", secretKey);
-            put("enabled", enabled);
+            put(SITE_KEY_PROPERTY, siteKey);
+            put(SECRET_KEY_PROPERTY, secretKey);
+            put(ENABLED_PROPERTY, enabled);
         }};
 
         ResourceResolver resolver = null;
@@ -64,7 +68,7 @@ public class RecaptchaConfigServlet extends SlingAllMethodsServlet {
         try {
             resolver = request.getResourceResolver();
             Resource recaptcha = resolver.getResource(PublickConstants.CONFIG_RECAPTCHA_PATH);
-    
+
             if (recaptcha != null) {
                 ModifiableValueMap properties = recaptcha.adaptTo(ModifiableValueMap.class);
                 properties.putAll(newProperties);
@@ -73,7 +77,7 @@ public class RecaptchaConfigServlet extends SlingAllMethodsServlet {
         } catch (PersistenceException e) {
             LOGGER.error("Could not save config settings.", e);
         }
-        
+
         response.sendRedirect(PublickConstants.ADMIN_CONFIG_PATH + ".html");
     }
 }
