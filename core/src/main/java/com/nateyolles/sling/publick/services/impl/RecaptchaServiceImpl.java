97ec0061ae9506750cac4124c8e9c69be3717f52
yolles <yolles@adobe.com>
2015-07-15 22:59:12 -0700

    reCAPTCHA service cleanup and comments

--- core/src/main/java/com/nateyolles/sling/publick/services/impl/RecaptchaServiceImpl.java
+++ core/src/main/java/com/nateyolles/sling/publick/services/impl/RecaptchaServiceImpl.java
@@ -1,88 +1,115 @@
 package com.nateyolles.sling.publick.services.impl;
 
-import java.io.InputStream;
 import java.util.Map;
-
-import javax.jcr.Node;
 
 import org.apache.felix.scr.annotations.Activate;
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Deactivate;
-import org.apache.felix.scr.annotations.Properties;
-import org.apache.felix.scr.annotations.Property;
 import org.apache.felix.scr.annotations.Reference;
 import org.apache.felix.scr.annotations.Service;
-import org.apache.sling.commons.osgi.PropertiesUtil;
-import org.apache.sling.api.SlingHttpServletRequest;
-import org.apache.sling.api.request.RequestParameter;
-import org.apache.sling.api.request.RequestParameterMap;
+
 import org.apache.sling.api.resource.LoginException;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.resource.ResourceResolverFactory;
 import org.apache.sling.api.resource.ValueMap;
+
 import org.osgi.service.component.ComponentContext;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.apache.jackrabbit.commons.JcrUtils;
-import org.apache.sling.jcr.resource.JcrResourceUtil;
 
 import com.nateyolles.sling.publick.PublickConstants;
 import com.nateyolles.sling.publick.services.RecaptchaService;
 
+/**
+ * reCAPTCHA service to get keys and communicate with Google
+ * to validate reCAPTCHA authentication. 
+ */
 @Service( value = RecaptchaService.class )
 @Component( metatype = true, immediate = true )
 public class RecaptchaServiceImpl implements RecaptchaService {
 
-  @Reference
-  private ResourceResolverFactory resourceResolverFactory;
+    /**
+     * ResourceResolver factory to get a ResourceRolver.
+     */
+    @Reference
+    private ResourceResolverFactory resourceResolverFactory;
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(RecaptchaServiceImpl.class);
+    /**
+     * The logger.
+     */
+    private static final Logger LOGGER = LoggerFactory.getLogger(RecaptchaServiceImpl.class);
 
-  public String getSiteKey() {
-      return getProperty("siteKey", String.class);
-  }
+    /**
+     * Get the reCAPTCHA site key.
+     *
+     * @return The reCAPTCHA site key.
+     */
+    public String getSiteKey() {
+        return getProperty("siteKey", String.class);
+    }
 
-  public String getSecretKey() {
-      return getProperty("secretKey", String.class);
-  }
+    /**
+     * Get the reCAPTCHA secret key.
+     *
+     * @return The reCAPTCHA secret key.
+     */
+    public String getSecretKey() {
+        return getProperty("secretKey", String.class);
+    }
 
-  public boolean getEnabled() {
-      return getProperty("enabled", Boolean.class);
-  }
+    public boolean getEnabled() {
+        return getProperty("enabled", Boolean.class);
+    }
 
-  @Activate
-  protected void activate(Map<String, Object> properties) {
-  }
+    /**
+     * Service activation.
+     */
+    @Activate
+    protected void activate(Map<String, Object> properties) {
+    }
 
-  @Deactivate
-  protected void deactivate(ComponentContext ctx) {
-  }
+    /**
+     * Service Deactivation.
+     *
+     * @param ctx The current component context.
+     */
+    @Deactivate
+    protected void deactivate(ComponentContext ctx) {
+    }
 
-  private <T> T getProperty(String propertyName, Class<T> type) {
-      T property = null;
-      ResourceResolver resolver = null;
+    /**
+     * Get the specified reCAPTCHA config property.
+     *
+     * @param propertyName The property name to get.
+     * @param type The property class type.
+     * @return The property value.
+     */
+    private <T> T getProperty(String propertyName, Class<T> type) {
+        T property = null;
+        ResourceResolver resolver = null;
 
-      try {
-          resolver = resourceResolverFactory.getAdministrativeResourceResolver(null);
+        try {
+            resolver = resourceResolverFactory.getAdministrativeResourceResolver(null);
 
-          if (resolver != null) {
-              Resource recaptcha = resolver.getResource(PublickConstants.CONFIG_RECAPTCHA_PATH);
-              
-              if (recaptcha != null) {
-                  ValueMap properties = recaptcha.adaptTo(ValueMap.class);
-                  property = properties.get(propertyName, type);
-              }
-          }
-       } catch (LoginException e) {
-           LOGGER.error("Could not get Resource Resolver", e);
-       } finally {
-           if (resolver != null && resolver.isLive()) {
-               resolver.close();
-               resolver = null;
-           }
-       }
+            if (resolver != null) {
+                Resource recaptcha = resolver.getResource(PublickConstants.CONFIG_RECAPTCHA_PATH);
 
-      return property;
-  }
+                if (recaptcha != null) {
+                    ValueMap properties = recaptcha.adaptTo(ValueMap.class);
+                    property = properties.get(propertyName, type);
+                }
+            }
+        } catch (LoginException e) {
+            LOGGER.error("Could not get Resource Resolver", e);
+        } finally {
+            if (resolver != null && resolver.isLive()) {
+                resolver.close();
+                resolver = null;
+            }
+        }
+
+        return property;
+    }
 }
