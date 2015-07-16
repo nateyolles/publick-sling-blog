c560cd1ac2665fa80744e8d465d223470b1d9587
yolles <yolles@adobe.com>
2015-07-15 23:19:46 -0700

    reCAPTCHA admin configuration Sightly component cleanup and comments

--- core/src/main/java/com/nateyolles/sling/publick/components/admin/RecaptchaConfig.java
+++ core/src/main/java/com/nateyolles/sling/publick/components/admin/RecaptchaConfig.java
@@ -6,18 +6,49 @@ import org.apache.sling.api.scripting.SlingBindings;
 import org.apache.sling.api.scripting.SlingScriptHelper;
 import org.apache.sling.scripting.sightly.pojo.Use;
 
-import com.nateyolles.sling.publick.PublickConstants;
 import com.nateyolles.sling.publick.services.RecaptchaService;
 
+/**
+ * Sightly component to update and save reCAPTCHA configurations. Two
+ * keys are provided by Google when you sign up for the reCAPTCHA
+ * service. The site key is delivered with the form to be submitted
+ * while the secret key is used in the server side communication with
+ * Google to verify the form submission wasn't hacked. It's not
+ * critical to keep the "secret" key as secure as password for example.
+ */
 public class RecaptchaConfig implements Use {
 
+    /**
+     * The reCAPTCHA service to save the config properties.
+     */
     private RecaptchaService recaptchaService;
 
+    /**
+     * The Sling Script Helper to get services.
+     */
     private SlingScriptHelper scriptHelper;
+
+    /**
+     * The reCAPTCHA site key.
+     */
     private String siteKey;
+
+    /**
+     * The reCAPTCHA secret key.
+     */
     private String secretKey;
+
+    /**
+     * Determines if the reCAPTCHA should be enabled even if the
+     * site and secret keys are provided.
+     */
     private boolean enabled;
 
+    /**
+     * Initialize the Sightly component.
+     *
+     * @param bindings The current execution context.
+     */
     @Override
     public void init(Bindings bindings) {
         scriptHelper = (SlingScriptHelper)bindings.get(SlingBindings.SLING);
@@ -31,14 +62,29 @@ public class RecaptchaConfig implements Use {
         }
     }
 
+    /**
+     * Get the reCAPTCHA site key.
+     *
+     * @return The reCAPTCHA site key.
+     */
     public String getSiteKey() {
         return siteKey;
     }
 
+    /**
+     * Get the reCAPTCHA secret key.
+     *
+     * @return The reCAPTCHA secret key.
+     */
     public String getSecretKey() {
         return secretKey;
     }
 
+    /**
+     * Is reCAPTCHA enabled.
+     *
+     * @return True if reCAPTCHA is enabled.
+     */
     public boolean getEnabled() {
         return enabled;
     }
